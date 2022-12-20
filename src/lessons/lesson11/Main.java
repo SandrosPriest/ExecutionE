e5de53b2faa7b55ae61ac711349da7e0ef245903
SandrosPriest <olespriest@gmail.com>
2022-12-20 12:31:23 +0300

    Init

--- src/lessons/lesson11/Main.java
+++ src/lessons/lesson11/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println(user.toString());
         System.out.println(user1.toString());
         System.out.println(user.equals(user));
+        System.out.println();
     }
 }
